c30999e1f0ca5a5b933599162b671155763f347a
Lillian Burnside <lillian.burnside@gmail.com>
2020-01-31 16:40:34 -0600

    fixed indents

--- src/components/CustomizeBody.js
+++ src/components/CustomizeBody.js
@@ -5,15 +5,22 @@ import CustomizeSection from './CustomizeSection'
 class CustomizeBody extends React.Component {
   render() {
   const features = Object.keys(this.props.features).map((feature, idx) => 
-  <CustomizeSection key={idx} feature ={feature} idx={idx} features = {this.props.features} 
-  state = {this.props.state} USCurrencyFormat = {this.props.USCurrencyFormat}
-  updateFeature = {this.props.updateFeature}/>);
-    return(
-      <form className="main__form">
-        <h2>Customize your laptop</h2>
-        {features}
-      </form>
-    )
+    <CustomizeSection 
+      key={idx} 
+      feature={feature} 
+      idx={idx} 
+      features={this.props.features} 
+      state={this.props.state} 
+      USCurrencyFormat={this.props.USCurrencyFormat}
+      updateFeature = {this.props.updateFeature}
+      />
+  );
+  return(
+    <form className="main__form">
+      <h2>Customize your laptop</h2>
+      {features}
+    </form>
+  )
   };
 }
 
