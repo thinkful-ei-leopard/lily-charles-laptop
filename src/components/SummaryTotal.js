cf2a6ab980e6cb91fb6dcbbe37676c444a00a2f0
Charles <charlesdpulliam@gmail.com>
2020-01-31 15:15:49 -0600

    Summary Working

--- src/components/SummaryTotal.js
+++ src/components/SummaryTotal.js
@@ -1,1 +1,18 @@
 import React from 'react';
+
+class SummaryTotal extends React.Component{
+  render(){
+    const total = Object.keys(this.props.state.selected).reduce(
+      (acc, curr) => acc + this.props.state.selected[curr].cost,
+      0
+    );
+    
+ return ( <div className="summary__total">
+ <div className="summary__total__label">Total</div>
+ <div className="summary__total__value">
+   {this.props.USCurrencyFormat.format(total)}
+ </div>
+</div>
+ )}
+}
+export default SummaryTotal
