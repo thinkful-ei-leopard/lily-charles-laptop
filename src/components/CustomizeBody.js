0e0bc74b8f731b4c3024cb405aaec2e70cf2e20b
Lillian Burnside <lillian.burnside@gmail.com>
2020-01-31 11:21:30 -0600

    started body

---
+++ src/components/CustomizeBody.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import slugify from 'slugify';
+
+
+class CustomizeBody extends React.Component {
+  render() {
+  const features = Object.keys(this.props.features).map((feature, idx) => {
+    const featureHash = feature + '-' + idx;
+    const options = this.props.features[feature].map(item => {
+      const itemHash = slugify(JSON.stringify(item));
+      return (
+        <div key={itemHash} className="feature__item">
+          <input
+            type="radio"
+            id={itemHash}
+            className="feature__option"
+            name={slugify(feature)}
+            checked={item.name === this.props.state.selected[feature].name}
+            onChange={e => this.props.updateFeature(feature, item)}
+          />
+          <label htmlFor={itemHash} className="feature__label">
+            {item.name} ({this.props.USCurrencyFormat.format(item.cost)})
+          </label>
+        </div>
+      );
+    });
+
+    return (
+      <fieldset className="feature" key={featureHash}>
+        <legend className="feature__name">
+          <h3>{feature}</h3>
+        </legend>
+        {options}
+      </fieldset>
+    );
+  });
+
+  
+    return(
+      <form className="main__form">
+        <h2>Customize your laptop</h2>
+        {features}
+      </form>
+    )
+  }
+}
+
+export default CustomizeBody
